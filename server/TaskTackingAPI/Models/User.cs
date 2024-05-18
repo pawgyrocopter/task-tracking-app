b29482a200026fb371cb88f0f44de9b862641b1d
pawgyrocopter <savely.volodkov@gmail.com>
2024-05-19 00:32:26 +0300

    Dependencies update.

--- server/TaskTackingAPI/Models/User.cs
+++ server/TaskTackingAPI/Models/User.cs
@@ -1,3 +1,4 @@
+using System;
 using Microsoft.AspNetCore.Identity;
 
 namespace TaskTackingAPI.Models;
--- server/TaskTackingAPI/Services/JwtService.cs
+++ server/TaskTackingAPI/Services/JwtService.cs
@@ -1,6 +1,9 @@
+using System;
+using System.Collections.Generic;
 using System.IdentityModel.Tokens.Jwt;
 using System.Security.Claims;
 using System.Text;
+using System.Threading.Tasks;
 using Microsoft.Extensions.Options;
 using Microsoft.IdentityModel.Tokens;
 using TaskTackingAPI.Models;
