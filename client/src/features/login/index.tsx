0ad4cd6f859c447adb1778d7dc798985104e8937
Dmytro <dima22022005@gmail.com>
2024-05-18 16:47:09 +0200

    Create ui only login component with login and registration forms.

---
+++ client/src/features/login/index.tsx
@@ -0,0 +1,79 @@
+import { useState } from 'react'
+
+const Login = () => {
+    const [isLoginForm, setIsLoginForm] = useState<boolean>(true)
+
+    function onChange(e: React.ChangeEvent<HTMLInputElement>) {
+        const { name, value } = e.target
+
+        switch (name) {
+            case 'username':
+                break
+            case 'password':
+                break
+        }
+    }
+
+    function onSubmit(e: React.ChangeEvent<HTMLFormElement>) {
+        e.preventDefault()
+        console.log(1)
+    }
+
+    return (
+        <div className="w-[20rem] bg-white h-[15rem] rounded-lg flex flex-col justify-center items-center">
+            <h1 className='mt-1'>{isLoginForm ? "Login" : "Register"}</h1>
+            <form
+                onSubmit={onSubmit}
+                className="w-full h-full flex flex-col items-center justify-center"
+            >
+                <div className="flex flex-col gap-2 h-[65%]">
+                    {!isLoginForm && (
+                        <input
+                            onChange={onChange}
+                            placeholder="email"
+                            id="email"
+                            className="border px-1 text-sm w-[16rem] h-[1.5rem] rounded-sm"
+                            type="email"
+                        />
+                    )}
+                    <input
+                        onChange={onChange}
+                        placeholder="name"
+                        id="name"
+                        className="border px-1 text-sm w-[16rem] h-[1.5rem] rounded-sm"
+                        type="text"
+                    />
+                    <input
+                        onChange={onChange}
+                        placeholder="password"
+                        id="password"
+                        className="border px-1 text-sm w-[16rem] h-[1.5rem] rounded-sm"
+                        type="password"
+                    />
+                    {!isLoginForm && (
+                        <input
+                            onChange={onChange}
+                            placeholder="repeat password"
+                            id="password"
+                            className="border px-1 text-sm w-[16rem] h-[1.5rem] rounded-sm"
+                            type="password"
+                        />
+                    )}
+                </div>
+                <div className="flex flex-col gap-1 h-[20%]">
+                    <button
+                        onClick={() => setIsLoginForm(!isLoginForm)}
+                        className="text-xs"
+                    >
+                        {isLoginForm
+                            ? "Haven't registered yet ?"
+                            : 'Already have an account ?'}
+                    </button>
+                    <button>Submit</button>
+                </div>
+            </form>
+        </div>
+    )
+}
+
+export default Login
