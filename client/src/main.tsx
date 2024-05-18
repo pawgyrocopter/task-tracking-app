428d4a02a768769c5da18339a3fc1be2b381677b
Dmytro <dima22022005@gmail.com>
2024-05-18 13:37:30 +0200

    Implement authentication.
    Make auth context to manage the state of authentication in the app.

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -3,9 +3,12 @@ import ReactDOM from 'react-dom/client'
 import '@/assets/css/main.css'
 import { RouterProvider } from 'react-router-dom'
 import { router } from './router'
+import { AuthProvider } from './context/AuthContext'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
     <React.StrictMode>
-        <RouterProvider router={router}></RouterProvider>
+        <AuthProvider>
+            <RouterProvider router={router}></RouterProvider>
+        </AuthProvider>
     </React.StrictMode>
 )
