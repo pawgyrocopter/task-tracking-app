df854e9095aa9ef957f91ea3271ca2fc4258e6eb
Dmytro <dima22022005@gmail.com>
2024-05-21 01:08:53 +0300

    Make redirect to a new page for creating project.

--- client/src/features/project/ProjectList.tsx
+++ client/src/features/project/ProjectList.tsx
@@ -1,6 +1,7 @@
 import useIsMobile from '@/hooks/useIsMobile'
 import ProjectCard from './Project'
 import { Project } from './types'
+import CreateProjectButton from './CreateProjectButton'
 
 const ProjectList = ({ projects }: { projects: Project[] }) => {
     const isMobile = useIsMobile()
@@ -9,14 +10,7 @@ const ProjectList = ({ projects }: { projects: Project[] }) => {
         <div className="w-full bg-gray-300 p-4 rounded-lg h-full flex flex-col gap-4">
             <div className="w-full flex flex-col md:flex-row gap-4 md:gap-0 justify-between items-center">
                 {isMobile && <h1 className="text-2xl">/Projects/</h1>}
-                <button className="rounded-xl w-full md:w-auto flex items-center gap-2 font-bold shadow-lg min-w-[14rem] px-2 py-2 bg-white">
-                    <img
-                        className="w-9 h-9"
-                        src="add-circle.svg"
-                        alt="Add circle icon"
-                    />
-                    <h1>Create new project</h1>
-                </button>
+                <CreateProjectButton />
                 {!isMobile && (
                     <>
                         <div className="flex-grow flex justify-center">
