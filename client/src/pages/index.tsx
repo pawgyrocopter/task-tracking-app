8a5a3cc3f32c5bc31fd98d9c6d4c238f97508a36
Dmytro <dima22022005@gmail.com>
2024-05-20 16:09:53 +0300

    Split forms into two separate components.

--- client/src/pages/index.tsx
+++ client/src/pages/index.tsx
@@ -1,13 +1,25 @@
 import { useAuth } from '@/context/AuthContext'
-import Login from '@/features/login'
+import Login from '@/features/forms/Login'
+import Registration from '@/features/forms/Registration'
+import { useState } from 'react'
 import { Navigate } from 'react-router-dom'
 
 const HomePage = () => {
     const { isAuthenticated } = useAuth()
 
+    const [isLoginForm, setIsLoginForm] = useState<boolean>(true)
+
+    if (isAuthenticated) {
+        return <Navigate to={'/projects'} />
+    }
+
     return (
         <div className="flex flex-col justify-center items-center h-full">
-            {!isAuthenticated ? <Login /> : <Navigate to={'/projects'} />}
+            {isLoginForm ? (
+                <Login setIsLoginForm={setIsLoginForm} />
+            ) : (
+                <Registration setIsLoginForm={setIsLoginForm} />
+            )}
         </div>
     )
 }
