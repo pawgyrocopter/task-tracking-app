d08cc84365f586df9adb41807ca943929faddc21
Dmytro <dima22022005@gmail.com>
2024-05-21 00:20:35 +0300

    Global style change.
    Add roboto font with thin, regular and bold styles. Change color palette of the application.

--- client/src/components/Navigation.tsx
+++ client/src/components/Navigation.tsx
@@ -1,6 +1,6 @@
 import { useEffect, useState } from 'react'
 import { useAuth } from '@/context/AuthContext'
-import { Link } from 'react-router-dom'
+import { Link, useLocation } from 'react-router-dom'
 import { Bars3Icon, XMarkIcon } from '@heroicons/react/20/solid'
 import useIsMobile from '@/hooks/useIsMobile'
 import { useParams } from 'react-router-dom'
@@ -9,6 +9,7 @@ const Navigation = () => {
     const { logout } = useAuth()
     const isMobile = useIsMobile()
     const [isMenuOpen, setIsMenuOpen] = useState(false)
+    const location = useLocation()
 
     const { projectId } = useParams()
 
@@ -28,6 +29,12 @@ const Navigation = () => {
         }
     }, [isMobile])
 
+    const getLinkClass = (path: string) => {
+        return location.pathname === path
+            ? 'underline text-black text-[1.35rem] font-bold'
+            : 'text-black text-[1.35rem]'
+    }
+
     return (
         <>
             {isMobile && (
@@ -37,23 +44,27 @@ const Navigation = () => {
                         onClick={toggleMenu}
                     >
                         {isMenuOpen ? (
-                            <XMarkIcon className="h-8 w-8 text-white" />
+                            <XMarkIcon className="h-8 w-8 text-black" />
                         ) : (
-                            <Bars3Icon className="h-8 w-8 text-white" />
+                            <Bars3Icon className="h-8 w-8 text-black" />
                         )}
                     </button>
                 </div>
             )}
 
             <aside
-                className={`md:static fixed top-0 left-0 z-[50] w-full md:w-[15rem] h-full bg-gray-800 text-white p-4 transform ${
+                className={`md:static fixed top-0 left-0 z-[50] w-full md:w-[15rem] h-full bg-gray-500 text-white p-4 transform ${
                     isMenuOpen ? 'translate-x-0' : '-translate-x-full'
                 } transition-transform duration-300 ease-in-out`}
             >
                 <nav className="h-full flex flex-col justify-between">
                     <ul className="flex flex-col items-center">
                         <li className="mb-4">
-                            <Link to="/projects" onClick={toggleMenu}>
+                            <Link
+                                to="/projects"
+                                onClick={toggleMenu}
+                                className={getLinkClass('/projects')}
+                            >
                                 Projects
                             </Link>
                         </li>
@@ -63,6 +74,9 @@ const Navigation = () => {
                                     <Link
                                         to={`/projects/${projectId}`}
                                         onClick={toggleMenu}
+                                        className={getLinkClass(
+                                            `/projects/${projectId}`
+                                        )}
                                     >
                                         Overview
                                     </Link>
@@ -71,6 +85,9 @@ const Navigation = () => {
                                     <Link
                                         to={`/projects/${projectId}/board`}
                                         onClick={toggleMenu}
+                                        className={getLinkClass(
+                                            `/projects/${projectId}/board`
+                                        )}
                                     >
                                         Board
                                     </Link>
@@ -78,12 +95,21 @@ const Navigation = () => {
                             </>
                         )}
                         <li>
-                            <Link to="/profile" onClick={toggleMenu}>
+                            <Link
+                                to="/profile"
+                                onClick={toggleMenu}
+                                className={getLinkClass('/profile')}
+                            >
                                 Profile
                             </Link>
                         </li>
                     </ul>
-                    <button onClick={logout}>Logout</button>
+                    <button
+                        className="p-2 bg-white text-[1.35rem] text-black shadow-lg rounded-full"
+                        onClick={logout}
+                    >
+                        Logout
+                    </button>
                 </nav>
             </aside>
         </>
--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -2,7 +2,17 @@
 export default {
     content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
     theme: {
-        extend: {},
+        fontFamily: {
+            roboto: ['Roboto', 'sans-serif'],
+        },
+        extend: {
+            colors: {
+                gray: {
+                    300: '#F8F8F8',
+                    500: '#D9D9D9',
+                },
+            },
+        },
     },
     plugins: [],
 }
