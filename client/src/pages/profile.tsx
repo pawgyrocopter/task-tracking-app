b2e7437427c4a3ccb7598ce8cc88461aa618717b
Dmytro <dima22022005@gmail.com>
2024-05-23 02:49:32 +0300

    Make basic profile page.

--- client/src/pages/profile.tsx
+++ client/src/pages/profile.tsx
@@ -1,5 +1,38 @@
+import Button from '@/components/ui/Button'
+
 const ProfilePage = () => {
-    return <></>
+    return (
+        <div className="md:w-[40rem] w-full md:items-start items-center h-full flex flex-col p-4 gap-4">
+            <img src="account-avatar.svg" className="w-[75px] md:w-[150px]" />
+            <form
+                onSubmit={(e) => e.preventDefault()}
+                className="w-full h-full flex flex-col items-center gap-2"
+            >
+                <input
+                    placeholder="Name"
+                    type="text"
+                    className="border px-2 placeholder:text-gray-700 text-lg w-full h-[2.25rem] rounded-lg shadow-lg"
+                />
+                <input
+                    placeholder="Email"
+                    type="text"
+                    className="border px-2 placeholder:text-gray-700 text-lg w-full h-[2.25rem] rounded-lg shadow-lg"
+                />
+                <input
+                    placeholder="Phone number"
+                    type="text"
+                    className="border px-2 placeholder:text-gray-700 text-lg w-full h-[2.25rem] rounded-lg shadow-lg"
+                />
+                <input
+                    placeholder="Password"
+                    type="password"
+                    className="border px-2 placeholder:text-gray-700 text-lg w-full h-[2.25rem] rounded-lg shadow-lg"
+                />
+
+                <Button className="w-[10rem] mt-[2rem]">Submit</Button>
+            </form>
+        </div>
+    )
 }
 
 export default ProfilePage
