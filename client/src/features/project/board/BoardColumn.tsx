2fd7ff51450b111208aabd69b05efd43ccad07a5
Dmytro <dmytrostoianov05@gmail.com>
2024-06-10 14:21:10 +0300

    Add column highlighting when dragging.

--- client/src/features/project/board/BoardColumn.tsx
+++ client/src/features/project/board/BoardColumn.tsx
@@ -3,8 +3,11 @@ import BoardTaskCard from './BoardTaskCard'
 import { BoardColumn as BoardColumnType, BoardTask } from './types'
 import AddTaskModal from './AddTaskModal'
 import { useProjectBoard } from '@/context/ProjectBoardContext'
+import { useState } from 'react'
 
 const BoardColumn = ({ column }: { column: BoardColumnType }) => {
+    const [isActive, setIsActive] = useState<boolean>(false)
+
     const {
         showModal,
         currentColumnId,
@@ -14,12 +17,32 @@ const BoardColumn = ({ column }: { column: BoardColumnType }) => {
         handleDragEnd,
     } = useProjectBoard()
 
+    const handleDragOver = (e: React.DragEvent<HTMLDivElement>) => {
+        e.preventDefault()
+        setIsActive(true)
+    }
+
+    const handleDragLeave = () => {
+        setIsActive(false)
+    }
+
+    const handleDragDrop = () => {
+        setIsActive(false)
+    }
+
     return (
-        <div className="flex w-full flex-col rounded-lg bg-gray-100 p-4 md:h-full md:w-1/3">
+        <div
+            className={`flex w-full flex-col rounded-lg p-4 md:h-full md:w-1/3 ${isActive ? 'bg-neutral-300' : 'bg-neutral-200'}`}
+        >
             <h2 className="mb-4 text-xl font-semibold">
                 {column.name} {column.tasks.length} of 21
             </h2>
-            <div className="md:flex-grow md:overflow-auto">
+            <div
+                onDragOver={handleDragOver}
+                onDragLeave={handleDragLeave}
+                onDrop={handleDragDrop}
+                className="md:flex-grow md:overflow-auto"
+            >
                 {column.tasks.map((task: BoardTask) => (
                     <BoardTaskCard
                         handleDragStart={handleDragStart}
