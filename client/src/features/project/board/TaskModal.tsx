c5b48f828369e6b819fe0967300050b5e9de19ea
Dmytro <dmytrostoianov05@gmail.com>
2024-06-12 01:31:18 +0300

    Create basic ui for modal.

--- client/src/features/project/board/TaskModal.tsx
+++ client/src/features/project/board/TaskModal.tsx
@@ -1,5 +1,7 @@
 import Modal from '@/components/ui/Modal'
 import { BoardTask } from './types'
+import { getPriorityColor } from './shared'
+import { capitalizeString } from '@/utils/strings'
 
 interface AddTaskModalProps {
     show: boolean
@@ -14,9 +16,31 @@ const TaskModal: React.FC<AddTaskModalProps> = ({ show, onClose, task }) => {
             show={show}
             onClose={onClose}
         >
-            <div className="flex h-full w-full flex-col items-center justify-center">
-                <div className="my-4 flex flex-col items-center justify-center">
-                    <h1 className="text-lg">{task.name}</h1>
+            <div className="flex h-full w-full flex-col items-center justify-between p-4">
+                <div className="flex flex-col items-center justify-center gap-4">
+                    <h1 className="text-xl font-semibold">{task.name}</h1>
+                    <p>{task.description}</p>
+                </div>
+                <div className="grid grid-cols-1 gap-2">
+                    <div className="flex items-center gap-2">
+                        <p className="w-[5rem]">Priority:</p>
+                        <div className="flex items-center gap-2">
+                            <p>{capitalizeString(task.priority)}</p>
+                            <div
+                                className={`h-3 w-3 rounded-full ${getPriorityColor(task.priority)}`}
+                            ></div>
+                        </div>
+                    </div>
+                    <div className="flex items-center gap-2">
+                        <p className="w-[5rem]">Assignee:</p>
+                        <div className="flex items-center gap-2">
+                            <p>{task.assignee}</p>
+                            <img
+                                src={task.avatar}
+                                className="flex h-6 w-6 items-center justify-center rounded-full"
+                            />
+                        </div>
+                    </div>
                 </div>
             </div>
         </Modal>
