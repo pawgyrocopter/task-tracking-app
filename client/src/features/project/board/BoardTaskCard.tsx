2995a503d71f4278e0c5499b0d8b92678eeff10d
Dmytro <dmytrostoianov05@gmail.com>
2024-06-12 01:31:18 +0300

    Create basic ui for modal.

--- client/src/features/project/board/BoardTaskCard.tsx
+++ client/src/features/project/board/BoardTaskCard.tsx
@@ -1,8 +1,9 @@
 import { motion } from 'framer-motion'
-import { BoardColumn, BoardTask, TaskPriority } from './types'
+import { BoardColumn, BoardTask } from './types'
 import DropIndicator from './DropIndicator'
 import TaskModal from './TaskModal'
 import { useState } from 'react'
+import { getPriorityColor } from './shared'
 
 const BoardTaskCard = ({
     column,
@@ -30,22 +31,21 @@ const BoardTaskCard = ({
                 onDragEnd={handleDragEnd}
                 onDragStart={() => handleDragStart(task)}
                 draggable={true}
-                className="cursor-pointer rounded-lg bg-white p-4 shadow-md active:cursor-grabbing"
+                className="flex cursor-pointer flex-col gap-2 rounded-lg bg-white p-4 shadow-md active:cursor-grabbing"
             >
                 <div className="flex items-center gap-3">
-                    <h3 className="mb-2 font-semibold">{task.name}</h3>
+                    <h3 className="font-semibold">{task.name}</h3>
                     <div
                         className={`h-3 w-3 rounded-full ${getPriorityColor(
                             task.priority
-                        )} mb-2`}
+                        )}`}
                     ></div>
                 </div>
                 <p className="text-sm text-gray-600">{task.description}</p>
-                <div className="flex space-x-2">
-                    <div className="flex h-6 w-6 items-center justify-center rounded-full bg-gray-200">
-                        {task.avatar}
-                    </div>
-                </div>
+                <img
+                    src={task.avatar}
+                    className="flex h-6 w-6 items-center justify-center rounded-full"
+                />
             </motion.div>
             {isLastCard && (
                 <DropIndicator beforeId={'-1'} columnId={column.id} />
@@ -57,21 +57,6 @@ const BoardTaskCard = ({
             />
         </>
     )
-}
-
-function getPriorityColor(priority: TaskPriority): string {
-    switch (priority) {
-        case 'HIGHEST':
-            return 'bg-red-500'
-        case 'HIGH':
-            return 'bg-orange-500'
-        case 'MEDIUM':
-            return 'bg-yellow-400'
-        case 'LOW':
-            return 'bg-green-400'
-        case 'LOWEST':
-            return 'bg-gray-400'
-    }
 }
 
 export default BoardTaskCard
