c04823bba3105d53b484706cf63f6d9ea9522e0f
Dmytro <dmytrostoianov05@gmail.com>
2024-06-18 00:08:03 +0200

    Implement get board data function.

--- client/src/services/project/ProjectService.ts
+++ client/src/services/project/ProjectService.ts
@@ -1,6 +1,7 @@
 import { customFetchWithCredentials } from '@/utils/fetch'
-import { ProjectDTO } from './types'
+import { ProjectDTO, TaskDTO } from './types'
 import { Project } from '@/features/project/types'
+import { BoardColumn, BoardTask } from '@/features/project/board/types'
 
 const PROJECTS_ENDPOINT = '/projects'
 
@@ -9,6 +10,54 @@ export async function getProjects(): Promise<Project[]> {
         await customFetchWithCredentials<ProjectDTO[]>(PROJECTS_ENDPOINT)
 
     return projectDTOsToProjects(projectDTOs)
+}
+
+export async function getBoardData(
+    projectId: string
+): Promise<BoardColumn[]> {
+    const taskDTOs = await customFetchWithCredentials<TaskDTO[]>(
+        `${PROJECTS_ENDPOINT}/${projectId}/tasks`
+    )
+
+    const columns = [
+        {
+            id: 0,
+            name: 'To Do',
+            tasks: taskDTOsToBoardTasks(
+                taskDTOs.filter((task) => task.state === 0)
+            ),
+        },
+        {
+            id: 1,
+            name: 'In Progress',
+            tasks: taskDTOsToBoardTasks(
+                taskDTOs.filter((task) => task.state === 1)
+            ),
+        },
+        {
+            id: 2,
+            name: 'Done',
+            tasks: taskDTOsToBoardTasks(
+                taskDTOs.filter((task) => task.state === 2)
+            ),
+        },
+    ]
+
+    return columns
+}
+
+function taskDTOsToBoardTasks(taskDTOs: TaskDTO[]): BoardTask[] {
+    return taskDTOs.map((taskDTO) => {
+        return {
+            id: taskDTO.id,
+            name: taskDTO.name,
+            description: taskDTO.description,
+            priority: taskDTO.priority,
+            avatar: '',
+            assignee: taskDTO.assigneeEmail,
+            dueDate: new Date(taskDTO.endDate),
+        }
+    })
 }
 
 function projectDTOsToProjects(projectDTOs: ProjectDTO[]): Project[] {
