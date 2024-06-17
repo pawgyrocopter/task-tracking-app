125c70a8591ee07f67b29c93e2ac8064b8074be0
Dmytro <dmytrostoianov05@gmail.com>
2024-06-18 00:08:03 +0200

    Implement get board data function.

--- client/src/pages/Board.tsx
+++ client/src/pages/Board.tsx
@@ -4,94 +4,9 @@ import {
 } from '@/context/ProjectBoardContext'
 import BoardColumn from '@/features/project/board/BoardColumn'
 import DeleteTaskArea from '@/features/project/board/DeleteTaskArea'
-import { useEffect } from 'react'
+import { getBoardData } from '@/services/project/ProjectService'
+import { useEffect, useState } from 'react'
 import { useParams } from 'react-router-dom'
-import { BoardColumn as BoardColumnType } from '@/features/project/board/types'
-
-const mockColumns: BoardColumnType[] = [
-    {
-        id: 0,
-        name: 'To Do',
-        tasks: [
-            {
-                id: '1',
-                name: 'To do 1',
-                description:
-                    'On hold. Before saving a new tournament we should...',
-                priority: 'LOWEST',
-                avatar: '/account-avatar.svg',
-                assignee: 'dima12345@gmail.com',
-            },
-            {
-                id: '2',
-                name: 'To do 2',
-                description:
-                    'On hold. Before saving a new tournament we should...',
-                priority: 'LOW',
-                avatar: '/account-avatar.svg',
-                assignee: 'dima12345@gmail.com',
-            },
-            {
-                id: '3',
-                name: 'To do 3',
-                description:
-                    'On hold. Before saving a new tournament we should...',
-                priority: 'HIGHEST',
-                avatar: '/account-avatar.svg',
-                assignee: 'dima12345@gmail.com',
-            },
-        ],
-    },
-    {
-        id: 1,
-        name: 'In Progress',
-        tasks: [
-            {
-                id: '4',
-                name: 'In Progress 1',
-                description:
-                    'On hold. Before saving a new tournament we should...',
-                priority: 'MEDIUM',
-                avatar: '/account-avatar.svg',
-                assignee: 'dima12345@gmail.com',
-            },
-            {
-                id: '5',
-                name: 'In Progress 2',
-                description:
-                    'On hold. Before saving a new tournament we should...',
-                priority: 'HIGH',
-                avatar: '/account-avatar.svg',
-                assignee: 'dima12345@gmail.com',
-            },
-            {
-                id: '6',
-                name: 'In Progress 1',
-                description:
-                    'On hold. Before saving a new tournament we should...',
-                priority: 'LOWEST',
-                avatar: '/account-avatar.svg',
-                assignee: 'dima12345@gmail.com',
-            },
-        ],
-    },
-    {
-        id: 2,
-        name: 'Done',
-        tasks: [
-            {
-                id: '7',
-                name: 'Done 1',
-                description:
-                    'On hold. Before saving a new tournament we should...',
-                priority: 'LOWEST',
-                avatar: '/account-avatar.svg',
-                assignee: 'dima12345@gmail.com',
-                dueDate: new Date(2024, 10, 21),
-            },
-        ],
-    },
-]
 
 const BoardPage = () => {
     return (
@@ -104,22 +19,37 @@ const BoardPage = () => {
 const BoardContent = () => {
     const { projectId } = useParams()
     const { columns, setColumns } = useProjectBoard()
+    const [isLoading, setIsLoading] = useState<boolean>(false)
 
     useEffect(() => {
         // make fetch and set columns with filtered tasks
-        console.log(projectId)
-        setColumns(mockColumns)
+        async function fetchBoardColumns() {
+            if (!projectId) {
+                return
+            }
+            setIsLoading(true)
+            const columns = await getBoardData(projectId)
+            setColumns(columns)
+            setIsLoading(false)
+        }
+        fetchBoardColumns()
     }, [projectId, setColumns])
 
     return (
-        <div className="relative flex h-full w-full flex-col">
-            <div className="flex flex-grow flex-col gap-4 overflow-y-scroll md:flex-row">
-                {columns?.map((column, idx) => (
-                    <BoardColumn key={idx} column={column} />
-                ))}
-            </div>
-            <DeleteTaskArea />
-        </div>
+        <>
+            {isLoading ? (
+                'Loading...'
+            ) : (
+                <div className="relative flex h-full w-full flex-col">
+                    <div className="flex flex-grow flex-col gap-4 overflow-y-scroll md:flex-row">
+                        {columns?.map((column, idx) => (
+                            <BoardColumn key={idx} column={column} />
+                        ))}
+                    </div>
+                    <DeleteTaskArea />
+                </div>
+            )}
+        </>
     )
 }
 
